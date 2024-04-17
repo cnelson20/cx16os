14a775be0181437b1a204ec68411eb8cb30057e7
cnelson20 <cjnelson2004@gmail.com>
2024-04-17 01:10:05 -0400

    actually commit the program

---
+++ programs/cat.s
@@ -0,0 +1,137 @@
+PRINT_STR = $9D09
+CHROUT = $9D03
+GET_HEX_NUM = $9D18
+GET_ARGS = $9D0F
+open_file = $9D1E
+close_file = $9D21
+read_file = $9D24
+
+r0L = $02
+r0H = $03
+r1L = $04
+r1H = $05
+
+init:
+	jsr GET_ARGS
+	stx $31
+	sta $30
+	sty argc	
+main:
+	dec argc
+	bne continue
+	rts
+continue:
+	
+	ldy #0
+	lda ($30), Y
+	beq found_end_word
+	
+	inc $30
+	bne continue
+	inc $31
+	jmp continue
+	
+found_end_word:
+	inc $30
+	bne @skip
+	inc $31
+@skip:
+	lda $30
+	ldx $31
+	ldy #0 ; read??
+	
+	jsr open_file
+	sta fd
+	cmp #$FF
+	beq file_error
+
+file_print_loop:	
+	lda #<buff
+	sta r0L
+	lda #>buff
+	sta r0H
+	
+	lda #128
+	sta r1L
+	lda #0
+	sta r1H
+	
+	lda fd
+	jsr read_file
+	sta bytes_read
+	
+	cpy #0
+	bne file_error_read
+	stz read_again
+	
+	cmp #128
+	bcc @print_read_bytes
+	
+	ldx #1
+	stx read_again
+@print_read_bytes:
+	ldx #0
+@print_read_bytes_loop:
+	lda buff, X
+	jsr CHROUT
+	inx
+	cpx bytes_read
+	bcc @print_read_bytes_loop
+	
+	lda read_again
+	bne file_print_loop
+	
+file_out_bytes:
+	lda #$d
+	jsr CHROUT
+
+	lda fd
+	jsr close_file
+	
+	jmp main
+	
+file_error_read:
+	tya
+	tax
+file_error:
+	stx fd
+	
+	lda #<error_msg_p1
+	ldx #>error_msg_p1
+	jsr PRINT_STR
+	
+	lda $30
+	ldx $31
+	jsr PRINT_STR
+	
+	lda #<error_msg_p2
+	ldx #>error_msg_p2
+	jsr PRINT_STR
+	
+	lda fd
+	jsr GET_HEX_NUM
+	jsr CHROUT
+	jsr CHROUT
+	
+	lda #$d
+	jsr CHROUT
+	
+	jmp main
+	
+fd:
+	.byte 0
+argc:
+	.byte 0
+read_again:
+	.byte 0
+bytes_read:
+	.byte 0
+
+error_msg_p1:
+	.asciiz "Error opening file '"
+
+error_msg_p2:
+	.asciiz "', code #:"
+
+buff:
+	.res 128
