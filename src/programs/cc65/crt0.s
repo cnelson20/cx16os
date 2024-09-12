6419d78dce8629296f7af13820f364c4fd27d0c1
cnelson20 <cjnelson2004@gmail.com>
2024-09-12 10:31:49 -0400

    add cfg and makefile for c programs

---
+++ src/programs/cc65/crt0.s
@@ -0,0 +1,47 @@
+.export   _init, _exit
+.import   _main
+
+.export   __STARTUP__ : absolute = 1        ; Mark as startup
+.import   __RAM_START__, __RAM_SIZE__       ; Linker generated
+
+.import    zerobss, initlib, donelib
+
+.setcpu "65816"
+.include "zeropage.inc"
+
+; ---------------------------------------------------------------------------
+; Place the startup code in a special segment
+
+.segment  "STARTUP"
+
+; ---------------------------------------------------------------------------
+; A little light 65c02 housekeeping
+
+_init:
+	; set stack pointer
+    stp
+	lda     #<(__RAM_START__ + __RAM_SIZE__)
+    sta     sp
+    lda     #>(__RAM_START__ + __RAM_SIZE__)
+    sta     sp+1
+
+	; Initialize memory storage
+    jsr     zerobss              ; Clear BSS segment
+    jsr     initlib              ; Run constructors
+
+	jsr     _main
+
+; ---------------------------------------------------------------------------
+; Back from main (this is also the _exit entry):
+
+_exit:
+	jsr     donelib              ; Run destructors	
+	rep #$10
+	.i16
+	ldx #$01FD
+	txs
+	sep #$10
+	.i8
+	
+	rts
+
