8b628f8fe92e27be8f61d28f95d9b5e1228b8cde
cnelson20 <cjnelson2004@gmail.com>
2024-09-16 23:43:56 -0400

    ctest still pwd, but uses extmem transfer to make sure routines work

--- src/programs/c/ctest.c
+++ src/programs/c/ctest.c
@@ -2,14 +2,30 @@
 #include <unistd.h>
 #include <string.h>
 
+#include "cx16os.h"
+
 //char hello_str[] = "Hello, World!";
 char pwd_buff[128];
 
-int main() {
-	getcwd(pwd_buff, 128);
-	puts(pwd_buff);
-	putchar('\r');
-    //write(1, hello_str, strlen(hello_str));
+unsigned char stp_rts[] = {0xDB, 0x60};
 
-    return 0;
+unsigned char bank;
+
+char *extmem_ptr = (char *)0xA000;
+unsigned short offset;
+
+int main() {
+	unsigned char c;
+	getcwd(pwd_buff, 128);
+
+	bank = res_extmem_bank();
+	memmove_extmem(bank, extmem_ptr, 0, pwd_buff, 128);
+	offset = 0;
+	set_extmem_rbank(bank);
+	while (c = read_byte_extmem(extmem_ptr, offset)) {
+		putchar(c);
+		++offset;
+	}
+	putchar('\r');
+	return 0;
 }
