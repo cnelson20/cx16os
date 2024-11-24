778f71fb412f6d3b7ab643ce3f2e6c1adc6b0a31
cnelson20 <cjnelson2004@gmail.com>
2024-11-23 23:21:27 -0500

    add tabs

--- src/osfiles/home/code/test.asm
+++ src/osfiles/home/code/test.asm
@@ -4,53 +4,53 @@
 
 .equ ptr0 $30
 
-;.byte $DB
-jsr get_args
-sta ptr0
-stx ptr0 + 1
-sty argc
+start:
+	jsr get_args
+	sta ptr0
+	stx ptr0 + 1
+	sty argc
 
 outer_loop:
-dec argc
-beq end
+	dec argc
+	beq end
 
-ldy #0
+	ldy #0
 loop:
-lda (ptr0), Y
-beq end_loop
-iny
-bne loop
+	lda (ptr0), Y
+	beq end_loop
+	iny
+	bne loop
 end_loop:
-iny
-tya
-clc
-adc ptr0
-sta ptr0
-lda ptr0 + 1
-adc #0
-sta ptr0 + 1
+	iny
+	tya
+	clc
+	adc ptr0
+	sta ptr0
+	lda ptr0 + 1
+	adc #0
+	sta ptr0 + 1
 
-lda ptr0
-ldx ptr0 + 1
-ldy #0
-jsr open_file
-pha
-jsr close_file
-pla
-cmp #$FF
-bne dont_overwrite
+	lda ptr0
+	ldx ptr0 + 1
+	ldy #0
+	jsr open_file
+	pha
+	jsr close_file
+	pla
+	cmp #$FF
+	bne dont_overwrite
 
-lda ptr0
-ldx ptr0 + 1
-ldy #'W'
-jsr open_file
-jsr close_file
+	lda ptr0
+	ldx ptr0 + 1
+	ldy #'W'
+	jsr open_file
+	jsr close_file
 
 dont_overwrite:
-jmp outer_loop
+	jmp outer_loop
 end:
-lda #0
-rts
+	lda #0
+	rts
 
 argc:
-.byte 0
+	.byte 0
