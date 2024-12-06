ca9967817c6f536e0249543dfeba5c59df1fb3c6
cnelson20 <cjnelson2004@gmail.com>
2024-12-06 16:53:17 -0500

    partially rewrite cowsay to support reading from stdin

--- src/programs/c/cowsay.c
+++ src/programs/c/cowsay.c
@@ -27,19 +27,50 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+
 #define MAX_WIDTH 40
 #define MAX(a, b) ((a > b) ? a : b)
 #define MIN(a, b) ((a < b) ? a : b)
+
+static unsigned char is_printable(char c) {
+	return ((c & 0x7F) >= 0x20) && (c != 0x7F);
+}
+
+// Returns the number of printable characters in str
+static int printable_strlen(char *str) {
+	static int len;
+	len = 0;
+	while (*str) {
+		if (is_printable(*str)) ++len;
+		++str;
+	}
+	return len;
+}
+
+static size_t printable_count(char *str, size_t count) {
+	static size_t l;
+	l = 0;
+	while (1) {
+		if (is_printable(*str)) {
+			if (!count) return l;
+			--count;
+		}
+		++str;
+		++l;
+	}
+}
 
 // Returns the length of the longst line of the message.
 static size_t LongestLineLength(int argc, char** argv) {
 	size_t max_len = 0;
 	size_t cur_line = 0;
-	int i;
 	
-	for (i = 0; i < argc; i++) {
-		size_t word_len = strlen(argv[i]) + 1;
+	for (; *argv; ++argv, --argc) {
+		char *str = *argv;
+		size_t word_len = printable_strlen(str) + 1;
 		// If the word itself is too long to fit in a line, then
 		// we return the maximum width.
 		if (word_len >= MAX_WIDTH)
@@ -60,24 +91,26 @@ static void PrintPaddedBreak(size_t pad) {
 	}
 	printf(" |\r");
 }
+
 // Prints the message
 static void PrintMessage(int argc, char** argv, size_t longest) {
 	size_t cur_line_len = 0;
-	int i;
-	for (i = 0; i < argc; i++) {
-		size_t word_len = strlen(argv[i]) + 1;
+	for (; *argv; ++argv, --argc) {
+		char* str = *argv;
+		size_t word_len = printable_strlen(str) + 1;
+		
 		if (cur_line_len == 0)
 			printf("| ");
 		// If it all fits in the line, then print the word and move on.
 		if (cur_line_len + word_len <= MAX_WIDTH) {
-			printf("%s ", argv[i]);
+			printf("%s ", str);
 			if (cur_line_len + word_len == MAX_WIDTH) {
 				PrintPaddedBreak(longest - cur_line_len - word_len);
 				cur_line_len = 0;
 				continue;
 			}
 			cur_line_len += word_len;
-			if (i == argc - 1)
+			if (argc == 1)
 				PrintPaddedBreak(longest - cur_line_len);
 			continue;
 		}
@@ -89,12 +122,11 @@ static void PrintMessage(int argc, char** argv, size_t longest) {
 		// If the word itself is too long, then we need to break it apart.
 		// Otherwise, we print the current word and move on.
 		if (word_len > MAX_WIDTH) {
-			char* str = argv[i];
 			size_t processed = 0;
 			size_t j;
 			for (j = 0; j <= word_len / MAX_WIDTH; j++) {
-				size_t len = MIN(MAX_WIDTH, strlen(str));
-				printf("%.*s", (int)len, str);
+				size_t len = MIN(MAX_WIDTH, printable_strlen(str));
+				printf("%.*s", printable_count(str, len), str);
 				PrintPaddedBreak(longest - len);
 				str += len;
 				processed += len;
@@ -104,30 +136,80 @@ static void PrintMessage(int argc, char** argv, size_t longest) {
 			}
 			cur_line_len = 0;
 		} else {
-			printf("%s ", argv[i]);
+			printf("%s ", str);
 			cur_line_len = word_len;
-			if (word_len == MAX_WIDTH || i == argc - 1) {
+			if (word_len == MAX_WIDTH || argc == 1) {
 				PrintPaddedBreak(longest - cur_line_len);
 			}
 		}
 	}
 }
+
+#define MAX_STDIN_WORDS 256
+
+char *stdin_argv[MAX_STDIN_WORDS + 1];
+
+#define READ_BUFF_SIZE 32
+
+char read_buff[READ_BUFF_SIZE];
+
+char **read_argv_from_stdin() {
+	static char **temp_argv;
+	static char *file_copy_buff;
+	static int bytes_read;
+	
+	file_copy_buff = malloc(1);
+	temp_argv = stdin_argv;
+	*temp_argv = file_copy_buff;
+	
+	while ((bytes_read = read(STDIN_FILENO, read_buff, READ_BUFF_SIZE)) > 0) {
+		static unsigned char i;
+		for (i = 0; i < bytes_read; ++i) {
+			if ('\r' == read_buff[i]) {
+				*file_copy_buff = '\0';
+				++file_copy_buff;
+				++temp_argv;
+				*temp_argv = file_copy_buff;
+			} else {
+				*file_copy_buff = read_buff[i];
+				++file_copy_buff;
+			}
+			if (file_copy_buff >= (char *)0xBFFF) goto out_of_mem;
+		}
+	}
+	out_of_mem:
+	*file_copy_buff = '\0';
+	temp_argv[1] = NULL;
+	return stdin_argv;
+}
+
 int main(int argc, char** argv) {
 	size_t bubble_width, i;
 	
-	for (i = 0; i < argc; ++i) {
-		if (!strcmp(argv[i], "-h")) {
-			printf("Usage: cowsay [message]\r");
-			return 0;
+	if (argc < 2) {
+		char **temp_argv;
+		temp_argv = argv = read_argv_from_stdin();
+		argc = 0;
+		for (; *temp_argv; ++temp_argv) {
+			++argc;
 		}
+	} else { // Read from stdin
+		for (i = 0; i < argc; ++i) {
+			if (!strcmp(argv[i], "-h")) {
+				printf("Usage: cowsay [message]\r");
+				return 0;
+			}
+		}
+		++argv;
+		--argc;
 	}
 	// No wordwrap because I'm too lazy.
-	bubble_width = LongestLineLength(argc - 1, argv + 1) + 1;
+	bubble_width = LongestLineLength(argc, argv) + 1;
 	printf(" _");
 	for (i = 0; i < bubble_width; i++)
 		printf("_");
 	printf(" \r");
-	PrintMessage(argc - 1, argv + 1, bubble_width - 1);
+	PrintMessage(argc, argv, bubble_width - 1);
 	printf(" -");
 	for (i = 0; i < bubble_width; i++)
 		printf("-");
