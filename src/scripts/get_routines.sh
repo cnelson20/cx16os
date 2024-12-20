f6a10b0d41cbc27c4faaaa0f65690e215d4c42d3
Cameron Nelson <cjnelson2004@gmail.com>
2024-12-20 15:13:53 -0500

    get_routines

--- src/scripts/get_routines.sh
+++ src/scripts/get_routines.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-awk "{ print \$1 }" programs/cc65/routines.inc | grep -E "^[a-z]" > /tmp/routines.txt
-awk "{print \$NF }" $1 | grep -E "[a-z0-9]$" > /tmp/prog.txt
+awk "{ print \$1 }" programs/cc65/routines.inc | grep -E "^[a-zA-Z0-9]" > /tmp/routines.txt
+awk "{print \$NF }" $1 | grep -E "[a-z0-9]+" > /tmp/prog.txt
 grep -Fwf /tmp/routines.txt /tmp/prog.txt | sort | uniq
