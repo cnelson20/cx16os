4ea019cc18ddfab6d8193a6f16ad444f6312c72a
cnelson20 <cjnelson2004@gmail.com>
2025-01-03 16:31:22 +0000

    prnfnt program

---
+++ src/programs/asm/prnfnt.s
@@ -0,0 +1,64 @@
+.include "routines.inc"
+.segment "CODE"
+
+NEWLINE = $0A
+
+r0 := $02
+
+start:
+	lda #<top_str
+	ldx #>top_str
+	jsr print_str
+	
+	jsr get_console_info
+	lda r0 + 1
+	cmp #40
+	bcs :+
+	lda #1
+	sta num_line_breaks
+	:
+	
+	
+	ldx #0
+loop:
+	txa
+	and #$0F
+	bne :++
+	lda #NEWLINE
+	ldy num_line_breaks
+	:
+	jsr CHROUT
+	dey
+	bne :-
+	txa
+	lsr A
+	lsr A
+	lsr A
+	lsr A
+	phx
+	jsr GET_HEX_NUM
+	txa
+	plx
+	jsr CHROUT
+	:
+	lda #' '
+	jsr CHROUT
+	lda #$80 ; print next char verbatim
+	jsr CHROUT
+	txa
+	jsr CHROUT
+	
+	inx
+	bne loop
+	
+	lda #NEWLINE
+	jsr CHROUT
+	
+	lda #0
+	rts
+
+num_line_breaks:
+	.byte 2
+
+top_str:
+	.asciiz "  0 1 2 3 4 5 6 7 8 9 A B C D E F"
