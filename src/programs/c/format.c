1de8246a426aad830c0df93839d2f3b685d09047
cnelson20 <cjnelson2004@gmail.com>
2025-01-28 23:07:10 +0000

    code

--- src/programs/c/format.c
+++ src/programs/c/format.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <ctype.h>
 
 #include <peekpoke.h>
 
@@ -28,6 +29,8 @@
 
 char *input_filename = NULL;
 
+char term_width = 80;
+
 void parse_options(int argc, char **argv);
 int main(int argc, char **argv);
 void usage(char status, char *optstr);
@@ -35,8 +38,16 @@ void usage(char status, char *optstr);
 void parse_file(int fd);
 int parse_line(unsigned linenum, char *line);
 
+void print_formatted_paragraph();
+void paragraph_add(char *lineptr);
+
 void parse_options(int argc, char **argv) {
 	(void)argc;
+	
+	// Get terminal width
+	__asm__ ("jsr %w", 0x9DB4); // get_console_info
+	__asm__ ("lda %b", 0x02); // r0 holds width of terminal
+	__asm__ ("sta %v", term_width);
 	
 	while (*(++argv)) {
 		static char c;
@@ -121,6 +132,8 @@ void parse_file(int fd) {
 		strcpy(line_buff, cptr + 1);
 		i = strlen(line_buff);
 	}
+	
+	paragraph_add(NULL);
  	
 	return;
 }
@@ -130,20 +143,72 @@ char *find_non_space(char *c) {
 	return c;
 }
 
-#define PARA_BUFF_SIZE 1024
+#define PARA_BUFF_SIZE 4096
 
-char paragraph_buff[PARA_BUFF_SIZE + 1] = {'\0'};
-size_t paragraph_buff_size = 0;
+unsigned char para_bank;
+char *paragraph_buff = (char *)0xA000;
+unsigned paragraph_buff_size = 0;
+
+#define TEMP_LINE_SIZE 80
+
+char temp_line_buff[TEMP_LINE_SIZE + 1];
+
+char print_spaces(char count) {
+	static char i;
+	for (i = 0; i != count; ++i) chrout(' ');
+	
+	return count;
+}
+
+#define TAB_WIDTH 8
 
 void print_formatted_paragraph() {
-	if (*paragraph_buff) {
+	if (paragraph_buff_size) {
+		static unsigned i;
 		
+		i = 0;
+		while (i < paragraph_buff_size) {
+			static int line_len;
+			static char *cur_ptr;
+			
+			POKEW(0x02, (unsigned)temp_line_buff);
+			POKEW(0x04, paragraph_buff_size);
+			POKEW(0x0A, 0x60DB);
+			__asm__ ("jsr %w", 0x000A);
+			
+			memmove_extmem(0, temp_line_buff, para_bank, paragraph_buff + i, term_width + 1);
+			line_len = 0;
+			cur_ptr = temp_line_buff;
+			
+			chrout('\t');
+			while ((cur_ptr - temp_line_buff) < paragraph_buff_size) {
+				int slen = strlen(cur_ptr);
+				if (line_len + slen >= term_width - TAB_WIDTH) break;
+				// else	
+				if (line_len) {
+					++line_len;
+					chrout(' ');
+				}
+				print_str(cur_ptr);
+				line_len += slen;
+				cur_ptr += slen + 1;
+			}
+			i += (cur_ptr - temp_line_buff);
+			
+		}
+		chrout('\n');
+		chrout('\n');
 	}
 	paragraph_buff[0] = '\0';
 	paragraph_buff_size = 0;
 }
 
 void paragraph_add(char *lineptr) {
+	if (!para_bank) {
+		para_bank = res_extmem_bank();
+		paragraph_buff_size = 0;
+	}
+	
 	if (!lineptr) {
 		// Flush output
 		print_formatted_paragraph();
@@ -152,21 +217,19 @@ void paragraph_add(char *lineptr) {
 		static char *strtok_arg;
 		
 		strtok_arg = lineptr;
-		while (*lineptr) {
-			char *word = strtok(strtok_arg, " \t");
+		while (1) {
+			char *word = strtok(strtok_arg, " \t\r\n");
+			if (!word) break;
 			strtok_arg = NULL;
 			
-			if (!word) break;
+			//printf("word: %s\n", word);
 			
 			l = strlen(word); // strtok always returns non-empty tokens
 			// If word would overflow buffer, print out current buff and clear it
 			if (paragraph_buff_size + l >= PARA_BUFF_SIZE) print_formatted_paragraph();
-			strcpy(paragraph_buff + paragraph_buff_size, word);
-			paragraph_buff_size += l;
-			strcpy(paragraph_buff + paragraph_buff_size, " ");
-			++paragraph_buff_size;
 			
-			lineptr = find_non_space(lineptr);
+			memmove_extmem(para_bank, paragraph_buff + paragraph_buff_size, 0, word, l + 1);
+			paragraph_buff_size += l + 1;
 		}
 	}
 }
@@ -174,17 +237,34 @@ void paragraph_add(char *lineptr) {
 int parse_line(unsigned linenum, char *line) {
 	static char *ptr;
 	
-	printf("%u: %s\n", linenum, line);
+	(void)linenum;
 	
+	// printf("%d: %s\n", linenum, line);
+	
+	// What to do on an empty line
 	ptr = line;
-	while (*ptr && isspace(ptr)) ++ptr;
+	while (*ptr && isspace(*ptr)) ++ptr;
 	
 	// If the first char in the str is '.', it is a command
-	if (*ptr == '.') {
+	if (*ptr == '\0') {
 		paragraph_add(NULL);
-		
-	} else {
+	} else if (*ptr != '.') {
 		paragraph_add(ptr);
+	} else {
+		// directive
+		char *tok = strtok(++ptr, " \t\r\n");
+		
+		paragraph_add(NULL);	
+		if (!strcmp(tok, "I")) {
+			chrout(1);
+			while (tok = strtok(NULL, " \t\r\n")) {
+				chrout(' ');
+				print_str(tok);
+			}
+			print_str(" \x01\n\n");
+		} else {
+			errx(1, "error on line %u: invalid directive .%s\n", linenum, tok);
+		}
 	}
 	
 	return 0;
