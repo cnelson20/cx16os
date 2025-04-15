f1f9d21037a484fde99cac3c388cdeed7766ef48
cnelson20 <cjnelson2004@gmail.com>
2025-04-15 18:28:12 +0100

    comments

--- src/programs/c/ctest.c
+++ src/programs/c/ctest.c
@@ -11,22 +11,22 @@ char buff[512];
 int main() {
 	int fd = open("macbeth.txt", O_RDONLY);
 	
-	read(fd, buff, 512);
+	read(fd, buff, 512); // Advances offset by 512
 	
 	// Should print 512
 	printf("%ld\n", lseek(fd, 0, SEEK_CUR));
 	
-	lseek(fd, 100, SEEK_SET);
+	lseek(fd, 100, SEEK_SET); // Set offset to 100
 	
 	// Should print 100
 	printf("%ld\n", lseek(fd, 0, SEEK_CUR));
 	
-	lseek(fd, 100, SEEK_CUR);
+	lseek(fd, 100, SEEK_CUR); // Adv offset by 100
 	
 	// Should print 200
 	printf("%ld\n", lseek(fd, 0, SEEK_CUR));
 	
-	lseek(fd, -100, SEEK_END);
+	lseek(fd, -100, SEEK_END); // Set offset to file size - 100
 	
 	// Should print ~113550
 	printf("%ld\n", lseek(fd, 0, SEEK_CUR));
